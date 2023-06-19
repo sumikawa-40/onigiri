889804790b6145172a5fdc3fc2a27f01df02be69
sumikawa-40 <china.sumikawa@nifty.com>
2023-06-19 16:32:36 +0900

    Add: ログイン機能を実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   root 'static_pages#top'
 
+  get 'login', to: 'user_sessions#new'
+  post 'login', to: 'user_sessions#create'
+
   resources :users, only: %i[new create]
 end
