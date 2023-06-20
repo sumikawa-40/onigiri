66adbfbbbc407a63d343c1ad38cf4b16495d6dec
sumikawa-40 <china.sumikawa@nifty.com>
2023-06-20 21:08:10 +0900

    Add: タイムゾーン設定と日本語化設定の追加

--- config/application.rb
+++ config/application.rb
@@ -34,6 +34,14 @@ module Onigiri
     # Don't generate system test files.
     config.generators.system_tests = nil
 
+    # 日本語設定
+    config.i18n.default_locale = :ja
+    config.i18n.load_path += Dir[Rails.root.join('config/locales/**/*.{rb,yml}').to_s]
+    
+    #タイムゾーンの設定
+    config.time_zone = 'Tokyo'	
+    config.active_record.default_timezone = :local
+
     config.generators do |g|
       g.skip_routes true
       g.assets false
