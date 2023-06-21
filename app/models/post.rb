34736babb9e3b5bc40577a0b60666112f55da1f7
sumikawa-40 <china.sumikawa@nifty.com>
2023-06-21 14:36:20 +0900

    Add: バリデーションとアソシエーションを追加

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :user
+
+  validates :title, presence: true, length: { maximum: 255 }
+  validates :image, presence: true
+  validates :content, length: { maximum: 65_535 }
 end
