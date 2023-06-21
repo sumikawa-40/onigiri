687dc4727491b63763690676a6f67a919a61c3a8
sumikawa-40 <china.sumikawa@nifty.com>
2023-06-21 14:30:48 +0900

    Add: postsテーブルを作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_19_050046) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_21_050102) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title", null: false
+    t.string "image", null: false
+    t.text "content"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name", null: false
@@ -26,4 +36,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_19_050046) do
     t.index ["name"], name: "index_users_on_name", unique: true
   end
 
+  add_foreign_key "posts", "users"
 end
