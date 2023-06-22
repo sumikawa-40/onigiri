811e01a0388af2eb93c2a900aa11ce5474d91645
sumikawa-40 <china.sumikawa@nifty.com>
2023-06-22 13:38:37 +0900

    Add: 投稿作成機能を追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,3 +1,25 @@
 class PostsController < ApplicationController
-  @posts = Post.all.includes(:user).order(created_at: :desc)
+  def index
+    @posts = Post.all.includes(:user).order(created_at: :desc)
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = current_user.posts.build(post_params)
+    if @post.save
+      redirect_to posts_path, success: t('defaults.message.created', item: Post.model_name.human)
+    else
+      flash.now['danger'] = t('defaults.message.not_created', item: Post.model_name.human)
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :content, :image)
+  end
 end
